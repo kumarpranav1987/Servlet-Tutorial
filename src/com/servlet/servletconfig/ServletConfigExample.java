413537e7abad7695a61de4aee3f0e91e803b25fe
pranav.kumar@iongroup.com <pranav.kumar@iongroup.com>
2019-03-29 17:47:35 +0530

    ServletConfig and ServletContext

---
+++ src/com/servlet/servletconfig/ServletConfigExample.java
@@ -0,0 +1,20 @@
+package com.servlet.servletconfig;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletConfigExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String coursename = getServletConfig().getInitParameter("coursename");
+		PrintWriter out = resp.getWriter();
+		out.append(coursename);
+	}
+}
---
+++ src/com/servlet/servletcontext/ServletContextExample.java
@@ -0,0 +1,23 @@
+package com.servlet.servletcontext;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletContextExample extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ServletContext ctx = getServletContext();
+		String name = ctx.getInitParameter("companyname");
+		PrintWriter out = resp.getWriter();
+		out.append("Compant Name=" + name);
+	}
+}
