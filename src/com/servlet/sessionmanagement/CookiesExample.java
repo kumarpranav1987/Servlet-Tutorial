d0d3cc6b33e3590cb8106041b0e63d3ed46b6208
Pranav Kumar <kumarpranav1987@gmail.com.com>
2019-04-01 20:29:40 +0530

    Commit

--- src/com/servlet/sessionmanagement/CookiesExample.java
+++ src/com/servlet/sessionmanagement/CookiesExample.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,10 +16,18 @@ public class CookiesExample extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setContentType("text/html");
 		String name = request.getParameter("username");
+		Cookie cookie = new Cookie("myname", name);
+		response.addCookie(cookie);
+		
 		PrintWriter out = response.getWriter();
 		out.append("Hello " + name);
-		response.sendRedirect("button.html");
+		
+		out.append("<form action=\"OtherServlet\">");
+		out.append("<input type=\"submit\" value=\"Go to OtherServlet\">");
+		out.append("</form>");
+		
 	}
 
 }
--- src/com/servlet/sessionmanagement/OtherServlet.java
+++ src/com/servlet/sessionmanagement/OtherServlet.java
@@ -1,8 +1,11 @@
 package com.servlet.sessionmanagement;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/OtherServlet")
 public class OtherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Cookie[] cookies = request.getCookies();
+		PrintWriter out = response.getWriter();
+		if (cookies != null) {
+			for (Cookie c : cookies) {
+				if (c.getName().equals("myname")) {
+					out.append("Your name is " + c.getValue());
+				}
+			}
+		}
+
 	}
 }
